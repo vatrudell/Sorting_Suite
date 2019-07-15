df6d7bf82476ac2ca4ee1e442114bcf75a3ffc04
Valerie Trudell <valedithat@pm.me>
2019-07-15 18:00:26 +0000

    completed buble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,13 @@
 class BubbleSort
-  def sort(list)
-    length = list.length
-    (length-1).times do |bubble|
-      (length-1).times do |index|
-        swap(list,index)
+  def self.sort(list)
+    length = list.length-1
+    length.times do
+      length.times do |index|
+        if list[index+1] < list[index]
+          list[index], list[index+1] = list[index+1], list[index]
+        end
       end
     end
     list
-  end
-
-  def swap(list,index)
-    if list[index + 1] < list[index]
-      list[index], list[index+1] = list[index+1], list[index]
-    end
   end
 end
--- bubble_sort_test.rb
+++ bubble_sort_test.rb
@@ -4,45 +4,37 @@ require_relative 'bubble_sort'
 
 class BubbleSortTest < Minitest::Test
   def test_array
-    sorter = BubbleSort.new
     array = [ 2, 4, 1, 3 ]
-    assert_equal [ 1, 2, 3, 4 ], sorter.sort(array)
+    assert_equal [ 1, 2, 3, 4 ], BubbleSort.sort(array)
   end
 
   def test_alphebet_array
-    sorter = BubbleSort.new
     array = [ "d", "a", "b", "c" ]
-    assert_equal [ "a", "b", "c", "d" ], sorter.sort(array)
+    assert_equal [ "a", "b", "c", "d" ], BubbleSort.sort(array)
   end
 
   def test_large_array
-    sorter = BubbleSort.new
     array = [ 4, 5, 2, 1, 4, 5, 8, 9, 0 ]
-    assert_equal array.sort, sorter.sort(array)
+    assert_equal array.sort, BubbleSort.sort(array)
   end
 
   def test_a_single_number_array
-    sorter = BubbleSort.new
-    assert_equal ["1"], sorter.sort(["1"])
+    assert_equal ["1"], BubbleSort.sort(["1"])
   end
 
   def test_a_single_letter_array
-    sorter = BubbleSort.new
-    assert_equal ["a"], sorter.sort(["a"])
+    assert_equal ["a"], BubbleSort.sort(["a"])
   end
 
   def test_it_sorts_an_array_with_two_elements
-		sorter = BubbleSort.new
-		assert_equal ["1", "2"], sorter.sort(["2", "1"])
+    assert_equal ["1", "2"], BubbleSort.sort(["2", "1"])
 	end
 
   def test_an_array_with_three_elements
-		sorter = BubbleSort.new
-		assert_equal ["1", "2", "3"], sorter.sort(["3", "2", "1"])
+		assert_equal ["1", "2", "3"], BubbleSort.sort(["3", "2", "1"])
 	end
 
-	def test_empty_array
-		sorter = BubbleSort.new
-		assert_equal [], sorter.sort([])
+  def test_empty_array
+		assert_equal [], BubbleSort.sort([])
 	end
 end
